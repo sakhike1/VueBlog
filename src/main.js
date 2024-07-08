f3355966a86092dc37c4a75ccfa896e0b05793d6
Sakhile Simelane <51701132+sakhike1@users.noreply.github.com>
2024-07-08 14:25:09 +0200

    updated pages

--- src/main.js
+++ src/main.js
@@ -2,9 +2,10 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 import './input.css';
-import store from './store/store'; // Adjust the path if necessary
+import store from './store/store';
+ // Adjust the path if necessary
 
 createApp(App)
   .use(store)
   .use(router)
-  .mount('#app');
+  .mount('#app')
