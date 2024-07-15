488b55f48d673a54c90a0226206ba01147469a28
Sakhile Simelane <51701132+sakhike1@users.noreply.github.com>
2024-07-16 00:13:37 +0200

    added login page

--- src/components/common/helloWorld.vue
+++ src/components/common/helloWorld.vue
@@ -1,7 +1,133 @@
 <template>
-    <div>
-        hello my friend
-    </div>
+    <section class="relative flex flex-col items-center justify-center overflow-hidden bg-gray-950">
+        <div class="shine-overlay"></div>
+        <svg class="absolute w-full h-full lg:w-auto lg:h-auto" viewBox="0 0 1681 1268" fill="none"
+            xmlns="http://www.w3.org/2000/svg">
+            <g filter="url(#filter0_f_1444_23691)">
+                <path
+                    d="M797.509 1376.37C1005.5 1320.64 1187.74 1228.6 1313.76 1120.32C1433.75 1017.23 1529.96 873.405 1489 720.528C1448.03 567.65 1292.8 491.201 1137.34 461.913C974.063 431.154 770.218 442.565 562.23 498.295C354.242 554.025 172.002 646.065 45.9808 754.342C-74.01 857.437 -170.22 1001.26 -129.257 1154.14C-88.2933 1307.01 66.9384 1383.46 222.401 1412.75C385.677 1443.51 589.521 1432.1 797.509 1376.37Z"
+                    stroke="url(#paint0_angular_1444_23691)" stroke-width="200"></path>
+            </g>
+            <g filter="url(#filter1_f_1444_23691)">
+                <path
+                    d="M797.501 1376.37C1005.49 1320.64 1187.73 1228.6 1313.75 1120.32C1433.74 1017.23 1529.95 873.405 1488.99 720.528C1448.02 567.65 1292.79 491.201 1137.33 461.913C974.055 431.154 770.21 442.565 562.222 498.295C354.234 554.025 171.994 646.065 45.973 754.342C-74.0178 857.437 -170.228 1001.26 -129.264 1154.14C-88.3011 1307.01 66.9306 1383.46 222.393 1412.75C385.669 1443.51 589.513 1432.1 797.501 1376.37Z"
+                    stroke="url(#paint1_angular_1444_23691)" stroke-width="200"></path>
+            </g>
+            <g filter="url(#filter2_f_1444_23691)">
+                <path
+                    d="M1272.85 196.642C1117.47 41.2578 943.78 -76.6173 783.354 -138.009C703.324 -168.635 619.849 -187.785 539.88 -185.623C458.828 -183.431 372.728 -158.586 306.228 -92.0857C239.728 -25.5856 214.882 60.5139 212.691 141.566C210.529 221.535 229.679 305.011 260.305 385.04C321.696 545.467 439.571 719.156 594.956 874.54C750.34 1029.92 924.03 1147.8 1084.46 1209.19C1164.49 1239.82 1247.96 1258.97 1327.93 1256.81C1408.98 1254.61 1495.08 1229.77 1561.58 1163.27C1628.08 1096.77 1652.93 1010.67 1655.12 929.616C1657.28 849.648 1638.13 766.172 1607.51 686.142C1546.11 525.716 1428.24 352.027 1272.85 196.642Z"
+                    stroke="url(#paint2_angular_1444_23691)" stroke-width="300"></path>
+            </g>
+            <g style="mix-blend-mode: overlay" filter="url(#filter3_f_1444_23691)">
+                <path
+                    d="M1018.52 1435.59C1226.51 1379.86 1408.75 1287.82 1534.77 1179.55C1654.76 1076.45 1750.97 932.629 1710.01 779.751C1669.05 626.874 1513.82 550.425 1358.35 521.137C1195.08 490.377 991.234 501.788 783.246 557.519C575.257 613.249 393.017 705.289 266.996 813.565C147.006 916.661 50.7955 1060.48 91.759 1213.36C132.722 1366.24 287.954 1442.69 443.417 1471.98C606.692 1502.73 810.537 1491.32 1018.52 1435.59Z"
+                    stroke="url(#paint3_angular_1444_23691)" stroke-width="200"></path>
+            </g>
+            <defs>
+                <filter id="filter0_f_1444_23691" x="-439.07" y="145.188" width="2237.88" height="1584.29"
+                    filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+                    <feFlood flood-opacity="0" result="BackgroundImageFix"></feFlood>
+                    <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape"></feBlend>
+                    <feGaussianBlur stdDeviation="100" result="effect1_foregroundBlur_1444_23691"></feGaussianBlur>
+                </filter>
+                <filter id="filter1_f_1444_23691" x="-259.078" y="325.188" width="1877.88" height="1224.29"
+                    filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+                    <feFlood flood-opacity="0" result="BackgroundImageFix"></feFlood>
+                    <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape"></feBlend>
+                    <feGaussianBlur stdDeviation="10" result="effect1_foregroundBlur_1444_23691"></feGaussianBlur>
+                </filter>
+                <filter id="filter2_f_1444_23691" x="-137.477" y="-535.788" width="2142.76" height="2142.76"
+                    filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+                    <feFlood flood-opacity="0" result="BackgroundImageFix"></feFlood>
+                    <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape"></feBlend>
+                    <feGaussianBlur stdDeviation="100" result="effect1_foregroundBlur_1444_23691"></feGaussianBlur>
+                </filter>
+                <filter id="filter3_f_1444_23691" x="-218.055" y="204.411" width="2237.88" height="1584.29"
+                    filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+                    <feFlood flood-opacity="0" result="BackgroundImageFix"></feFlood>
+                    <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape"></feBlend>
+                    <feGaussianBlur stdDeviation="100" result="effect1_foregroundBlur_1444_23691"></feGaussianBlur>
+                </filter>
+                <radialGradient id="paint0_angular_1444_23691" cx="0" cy="0" r="1" gradientUnits="userSpaceOnUse"
+                    gradientTransform="translate(679.87 937.332) rotate(75) scale(354.525 737.669)">
+                    <stop offset="0.140936" stop-color="#5840FA"></stop>
+                    <stop offset="0.336288" stop-color="#BFDEF8"></stop>
+                    <stop offset="0.44736" stop-color="#FF696A"></stop>
+                    <stop offset="0.654315" stop-color="#FF696A"></stop>
+                    <stop offset="0.806766" stop-color="#BFDEF8"></stop>
+                    <stop offset="1" stop-color="#5840FA"></stop>
+                </radialGradient>
+                <radialGradient id="paint1_angular_1444_23691" cx="0" cy="0" r="1" gradientUnits="userSpaceOnUse"
+                    gradientTransform="translate(679.862 937.332) rotate(75) scale(354.525 737.669)">
+                    <stop offset="0.140936" stop-color="#5840FA"></stop>
+                    <stop offset="0.336288" stop-color="#BFDEF8"></stop>
+                    <stop offset="0.44736" stop-color="#FF696A"></stop>
+                    <stop offset="0.654315" stop-color="#FF696A"></stop>
+                    <stop offset="0.806766" stop-color="#BFDEF8"></stop>
+                    <stop offset="1" stop-color="#5840FA"></stop>
+                </radialGradient>
+                <radialGradient id="paint2_angular_1444_23691" cx="0" cy="0" r="1" gradientUnits="userSpaceOnUse"
+                    gradientTransform="translate(933.905 535.591) rotate(-45) scale(329.346 737.669)">
+                    <stop offset="0.140936" stop-color="#5840FA"></stop>
+                    <stop offset="0.336288" stop-color="#BFDEF8"></stop>
+                    <stop offset="0.44736" stop-color="#FF696A"></stop>
+                    <stop offset="0.654315" stop-color="#FF696A"></stop>
+                    <stop offset="0.806766" stop-color="#BFDEF8"></stop>
+                    <stop offset="1" stop-color="#5840FA"></stop>
+                </radialGradient>
+                <radialGradient id="paint3_angular_1444_23691" cx="0" cy="0" r="1" gradientUnits="userSpaceOnUse"
+                    gradientTransform="translate(900.885 996.556) rotate(75) scale(354.525 737.669)">
+                    <stop offset="0.140936" stop-color="#5840FA"></stop>
+                    <stop offset="0.336288" stop-color="#BFDEF8"></stop>
+                    <stop offset="0.44736" stop-color="#FF696A"></stop>
+                    <stop offset="0.654315" stop-color="#FF696A"></stop>
+                    <stop offset="0.806766" stop-color="#BFDEF8"></stop>
+                    <stop offset="1" stop-color="#5840FA"></stop>
+                </radialGradient>
+            </defs>
+        </svg>
+        <div class="relative items-center px-8 py-12 mx-auto md:px-12 lg:px-32 max-w-7xl">
+            <div class="max-w-4xl mx-auto">
+
+                <h2 class="mt-6 text-3xl font-medium tracking-tight text-white lg:text-4xl">
+                    Thank you for signing up<span class="block text-white">
+                        Good to have you on my blog </span>
+                </h2>
+            </div>
+            <div class="w-full px-5 text-white py-24 mx-auto lg:px-32">
+                <div class="flex flex-col w-full mx-auto mb-2 prose text-left prose-md">
+
+                    <p>Welcome to my blog, where I share insights and updates about the Vue.js ecosystem. Vue.js, a
+                        progressive JavaScript framework, is designed to be incrementally adoptable and focuses on
+                        building user interfaces. Whether you're a beginner or an experienced developer, there's always
+                        something new to learn in the Vue.js world. To get the most out of Vue.js, it's important to
+                        follow best practices. This includes writing clean and modular code, leveraging Vue's reactivity
+                        system effectively, and utilizing Vue Router
+                        and Vuex to manage complex state and routing. Regularly updating dependencies and staying
+                        informed about new features and updates can also help maintain a healthy and robust codebase.
+
+                        The Vue.js community is vibrant and supportive, with numerous resources available for learning
+                        and troubleshooting. The official Vue.js documentation is comprehensive and well-maintained,
+                        providing detailed guides and API references. Additionally, there are many tutorials, courses,
+                        and forums where developers can share knowledge and seek advice.
+                        For those looking to delve deeper, advanced topics in Vue.js include server-side rendering with
+                        Nuxt.js, building static sites with Gridsome, and optimizing performance with techniques like
+                        lazy loading and code splitting. Understanding these advanced concepts can help developers build
+                        high-performance, scalable applications.
+                        Stay tuned for more updates and tutorials on the Vue.js ecosystem. If you have any questions or
+                        need assistance, feel free to contact me. Together, we can explore the full potential of Vue.js
+                        and create amazing applications.</p>
+                </div>
+                <div class="mt-8 text-left">
+                    <router-link to="/Contact"
+                        class="px-6 py-3 text-lg font-medium text-white bg-[radial-gradient(ellipse_at_bottom,_var(--tw-gradient-stops))] from-amber-200 via-violet-600 to-sky-900 hover:bg-blue-700">Contact
+                        Me</router-link>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
 </template>
 
 <script>
@@ -10,4 +136,30 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style scoped>
+@keyframes background-shine {
+    0% {
+        background-position: 0 0;
+    }
+
+    100% {
+        background-position: 100% 0;
+    }
+}
+
+.relative {
+    position: relative;
+}
+
+.shine-overlay {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: linear-gradient(110deg, rgba(12, 12, 12, 0.712) 45%, rgba(19, 18, 18, 0.836) 55%, rgba(15, 15, 15, 0.637));
+    background-size: 200% 100%;
+    animation: background-shine 3s linear infinite;
+    pointer-events: none;
+}
+</style>
